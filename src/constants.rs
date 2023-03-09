171b5709d742e9208fd3042b427834df9db02f27
logiczsniper <lczernel@gmail.com>
2023-03-09 21:39:26 +0000

    Learned how to set color of text and use LinearLayout

---
+++ src/constants.rs
@@ -0,0 +1,10 @@
+use cursive::theme::Color;
+
+pub const OTHELLO: &str = "オセロ";
+pub const CHIP_OUTLINE: &str = "○";
+pub const CHIP_FILLED: &str = "●";
+
+pub const BLACK: Color = Color::Rgb(0, 0, 5);
+pub const WHITE: Color = Color::Rgb(255, 255, 250);
+pub const GREEN: Color = Color::Rgb(144, 169, 89);
+pub const PINK: Color = Color::Rgb(176, 123, 172);
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+mod constants;
+
+use ::cursive::views::{Dialog, TextView};
+use cursive::{
+    theme::{Color, ColorStyle, Palette, Theme},
+    views::{Button, LinearLayout},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let mut siv = cursive::default();
+
+    let mut theme = Theme::terminal_default();
+    theme
+        .palette
+        .set_color("Background", Color::TerminalDefault);
+    theme.palette.set_color("Primary", constants::WHITE);
+    theme.palette.set_color("TitlePrimary", constants::PINK);
+    siv.set_theme(theme);
+
+    // let title = TextView::new(constants::OTHELLO)
+    //     .style();
+
+    let mut title = TextView::new(constants::OTHELLO);
+    title.set_style(ColorStyle::front(constants::PINK));
+
+    let mut player_one_chip = TextView::new(constants::CHIP_OUTLINE);
+    player_one_chip.set_style(ColorStyle::front(constants::GREEN));
+
+    let linear_layout = LinearLayout::vertical()
+        .child(title)
+        .child(player_one_chip)
+        .child(Button::new("Ok", |s| s.quit()));
+
+    siv.add_global_callback('q', |s| s.quit());
+    siv.add_layer(linear_layout);
+
+    siv.run();
 }
